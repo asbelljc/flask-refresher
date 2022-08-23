ff3252441ee700a4ebba5f61337e9c4e8ccf021e
Jonathan Asbell <asbelljc@gmail.com>
2022-08-22 22:27:55 -0400

    Added high-low guessing feature.

--- hello.py
+++ hello.py
@@ -1,45 +1,28 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 
+answer = random.randint(0, 9)
+
+
 @app.route("/")
-def hello_world():
-    return "The value of __name__ is {}".format(__name__)
+def home():
+    return (
+        "<h1>Guess a number between 0 and 9</h1>"
+        "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' />"
+    )
 
 
-def make_bold(function):
-    def wrapper():
-        return f"<b>{function()}</b>"
-
-    return wrapper
-
-
-def make_italic(function):
-    def wrapper():
-        return f"<em>{function()}</em>"
-
-    return wrapper
-
-
-def make_underlined(function):
-    def wrapper():
-        return f"<u>{function()}</u>"
-
-    return wrapper
-
-
-@app.route("/bye")
-@make_bold
-@make_italic
-@make_underlined
-def bye():
-    return "Bye!"
-
-
-@app.route("/username/<name>")
-def greet(name):
-    return f"Hello, {name}!"
+@app.route("/<int:number>")
+def guess(number):
+    if number == answer:
+        return "<h1>You got it!</h1>"
+    elif number > answer:
+        return "<h1>Too high! You suck!</h1>"
+    else:
+        return "<h1>Too low! What a moron!</h1>"
 
 
 if __name__ == "__main__":
