134eed0b7886773df15c87d60479f8df7e6ba97e
Jonathan Asbell <asbelljc@gmail.com>
2022-08-22 23:26:44 -0400

    Worked with API calls in jinja templating.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import random
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html", number=random.randint(0, 1000))
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    res_age = requests.get(f"http://api.agify.io?name={name}").json()
+    res_gender = requests.get(f"https://api.genderize.io?name={name}").json()
+    gender = "dude" if res_gender["gender"] == "male" else "lady"
+    name = name.capitalize()
+
+    return render_template("guess.html", name=name, age=res_age["age"], gender=gender)
 
 
 if __name__ == "__main__":
