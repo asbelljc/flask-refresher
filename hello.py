920f14935fa60f1bfe870592f55006df050f268a
Jonathan Asbell <asbelljc@gmail.com>
2022-08-22 22:15:22 -0400

    Added decorator practice.

--- hello.py
+++ hello.py
@@ -5,4 +5,42 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "The value of __name__ is {}".format(__name__)
+
+
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+
+    return wrapper
+
+
+def make_italic(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+
+    return wrapper
+
+
+@app.route("/bye")
+@make_bold
+@make_italic
+@make_underlined
+def bye():
+    return "Bye!"
+
+
+@app.route("/username/<name>")
+def greet(name):
+    return f"Hello, {name}!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
